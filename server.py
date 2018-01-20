26fbe2335d926574e012aa39eaa58039ffa1272b
Djapy <djapy@yandex.com>
2018-01-20 17:14:10 +0300

    Creates a flask application for the distribution of content through API

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, Response
+import tempfile
+from werkzeug.contrib.cache import FileSystemCache
+from parse_site.parser_elcom import output_products
+import json
+
+
+ONE_DAY = 60 * 60 * 24
+
+app = Flask(__name__)
+tmp_dir = tempfile.mkdtemp()
+cache = FileSystemCache(cache_dir=tmp_dir)
+
+
+def products_info_from_cache():
+    products_info = cache.get('products_info')
+    if products_info is None:
+        products_info = output_products()
+        cache.set('products_info', products_info, timeout=ONE_DAY)
+    return products_info
+
+
+@app.route('/api')
+def get_api():
+    return Response(json.dumps(products_info_from_cache(),
+                               indent=2,
+                               ensure_ascii=False),
+                    content_type='application/json; charset=utf-8')
+
+
+if __name__ == "__main__":
+    app.run()
